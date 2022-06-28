928e1879326e29f13db0b27f2d73387814240a84
glowredman <fredcraft00@gmail.com>
2022-06-28 22:51:45 +0200

    Import improvements

--- src/main/java/glowredman/defaultserverlist/ModContainer.java
+++ src/main/java/glowredman/defaultserverlist/ModContainer.java
@@ -1,5 +1,8 @@
 package glowredman.defaultserverlist;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -7,6 +10,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("defaultserverlist")
 public class ModContainer {
+	
+	public static final Logger LOGGER = LogManager.getLogger("defaultserverlist");
 	
 	public ModContainer() {
 		FMLJavaModLoadingContext.get().getModEventBus().register(this.getClass());
